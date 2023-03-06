905e21310f0ebc630ff8d17730567e3b78240afe
MisterMew <thebesteccentric@gmail.com>
2023-03-06 22:29:01 +1100

    Player Rework
    + Added some particles
    + Created slime ball with oscilaltion
    + Rework player character controller (WIP)

--- Obstacle Course/Assets/Scripts/Scripts Player/PlayerMovement.cs
+++ Obstacle Course/Assets/Scripts/Scripts Player/PlayerMovement.cs
@@ -3,10 +3,22 @@
 public class PlayerMovement : MonoBehaviour
 {
     /* Variables */
+    private Rigidbody rb;
+    [SerializeField] private float jumpVelocityFalloff = 0F;
+    [SerializeField] private float fallMultiplier = 0F;
+
+    /* Movement Variables */
     [SerializeField] private float moveSpeed = 0f;
     [SerializeField] private float jumpHeight = 0f;
-
     private float xValue, yValue, zValue = 0;
+
+    /* VFX Variables */
+    public ParticleSystem particle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,5 +28,29 @@ public class PlayerMovement : MonoBehaviour
         zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(xValue, yValue, zValue);
+
+        if (Input.GetButton("Jump")) CreateParticles();
+
+        if (rb.velocity.y < jumpVelocityFalloff || rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            rb.velocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.deltaTime;
     }
+
+    private void CreateParticles()
+    {
+        particle.Play();
+    }
+
+    //FEATURE: SPRINT
+    // Collect slime balls to increase in size (to a pre-defined maximum point)
+    // Sprinting consumes size but has a huge speed boost
+    // Sprinting emits a unique trail:
+    /// trailRenderer.emitting true/false
+
+    //FEATURE: JUMP
+    // Jumping uses no cost
+    // Double jumps consume size
+
+    //SIZING
+    // Consuming size emitts an echo effect
+    // Gaining size emits a pulse effect
 }
